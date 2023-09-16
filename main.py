007fe0f46c2d20c4aea6389a5f282ba897d65704
Kaushalya02 <145102168+Kaushalya02@users.noreply.github.com>
2023-09-16 19:54:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-from streamlit as st
-import detect
+import streamlit as str
+from langdetect import detect
 from textblob import TextBlob
-# Get the user input.
-lang_names = st.{"en":"English",
-              "hi":"Hindi",
-              "ur":"Urdu",
-              "af":"Afrikaans",
-              "ar":"Arabic",
-              "bn":"Bengali	",
-              "zh":"Chinese	",
-              "de":"German",
-              "it":"Italian",
-              "kn":"Kannada"}
-text = st.input("Enter a text: ")
+
+
+str.lang_names = {"en":"English",
+                "hi":"Hindi",
+                "ur":"Urdu",
+                "af":"Afrikaans",
+                "ar":"Arabic",
+                "bn":"Bengali",
+                "zh":"Chinese",
+                "de":"German",
+                "it":"Italian",
+                "kn":"Kannada"}
+str.text = input("Enter a text: ")
 # Detect the language of the text.
-lang = st.detect(text)
-text_blob = st.TextBlob(text)
+str.lang = detect(text)
+str.text_blob = TextBlob(text)
     # Get the sentiment polarity.
-sentiment = st.text_blob.sentiment.polarity
+str.sentiment = text_blob.sentiment.polarity
     # Convert the sentiment polarity to a sentiment category.
-st.if sentiment < 0:
-    sentiment_category = "negative"
-st.elif sentiment == 0:
-    sentiment_category = "neutral"
-st.else:
-    sentiment_category = "positive"
+if sentiment < 0:
+    str.sentiment_category = "negative"
+elif sentiment == 0:
+    str.sentiment_category = "neutral"
+else:
+    str.sentiment_category = "positive"
     # Return the sentiment category.
 # Return the language code and full name.
-st.print(f"Language code: {lang}")
-st.print(f"Language name: {lang_names[lang]}")
-st.print(f"Sentiment category: {sentiment_category}")
+print(f"Language code: {lang}")
+print(f"Language name: {lang_names[lang]}")
+print(f"Sentiment category: {sentiment_category}")
+
